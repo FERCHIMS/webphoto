7c5f1259e0ab9aab818e3036e72a7e319096a422
FERCHIMS <gr.egfredderick@gmail.com>
2024-10-01 12:53:12 -0300

    arreglo de responsive 1

--- src/components/titles/TitleBanner.jsx
+++ src/components/titles/TitleBanner.jsx
@@ -24,7 +24,7 @@ const TitleBanner = () => {
     },[])
 
   return (
-    <div className="overflow-hidden pt-10 z-50  w-fit  justify-start flex ">
+    <div className="overflow-hidden pt-[3rem] z-50  w-fit  justify-start flex ">
         <h2 ref={titleRef} className={`text-[3em] leading-[1em] sm:leading-0 sm:text-[7em] tracking-[0.01em] text-white transition-all duration-[1200ms] delay-200 font-bold ease-[cubic-bezier(0.25, 1, 0.5, 1)] ${ResolveThin.className} ${isInView ? "translate-y-0 opacity-100 " : "translate-y-[5vh] opacity-0"}  `} >CHRISTIAN JOHNS</h2>
     </div>
   )
--- src/components/toppage/TopPage.jsx
+++ src/components/toppage/TopPage.jsx
@@ -19,7 +19,7 @@ const TopPage = ({children, className=""}) => {
     }, [])
 
   return (
-    <div className={`fixed pl-20 bg-black z-40 w-full overflow-hidden transition-all duration-[1200ms] ease-[cubic-bezier(0.65,0.05,0.36,1)] ${isScrolled ? "h-[0vh] " : "h-screen"} `}>
+    <div className={`flex-1 sm:fixed pl-5 sm:pl-20 bg-black z-40 w-full overflow-hidden transition-all duration-[1200ms] ease-[cubic-bezier(0.65,0.05,0.36,1)] ${isScrolled ? "h-[0vh] " : "h-screen"} `}>
         {children}
     </div>
   )
