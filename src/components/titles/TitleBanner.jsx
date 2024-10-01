6da67bdfe39633434a28bfe6d73a1555956e2327
FERCHIMS <gr.egfredderick@gmail.com>
2024-10-01 12:06:39 -0300

    cambiando el responive

--- src/components/titles/TitleBanner.jsx
+++ src/components/titles/TitleBanner.jsx
@@ -24,8 +24,8 @@ const TitleBanner = () => {
     },[])
 
   return (
-    <div className="overflow-hidden z-50  w-fit  justify-start flex ">
-        <h2 ref={titleRef} className={`text-[3em] sm:text-[7em] tracking-[0.01em] text-white transition-all duration-[1200ms] delay-200 font-bold ease-[cubic-bezier(0.25, 1, 0.5, 1)] ${ResolveThin.className} ${isInView ? "translate-y-0" : "translate-y-[20vh]"} `} >CHRISTIAN JOHNS</h2>
+    <div className="overflow-hidden pt-10 z-50  w-fit  justify-start flex ">
+        <h2 ref={titleRef} className={`text-[3em] leading-[1em] sm:leading-0 sm:text-[7em] tracking-[0.01em] text-white transition-all duration-[1200ms] delay-200 font-bold ease-[cubic-bezier(0.25, 1, 0.5, 1)] ${ResolveThin.className} ${isInView ? "translate-y-0 opacity-100 " : "translate-y-[5vh] opacity-0"}  `} >CHRISTIAN JOHNS</h2>
     </div>
   )
 }
