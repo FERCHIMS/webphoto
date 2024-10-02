3af271073ca7bd974029363f0f9e395d6fbc959d
FERCHIMS <gr.egfredderick@gmail.com>
2024-10-02 13:55:58 -0300

    mejoras de drop

--- src/components/dropdown/DropDown.jsx
+++ src/components/dropdown/DropDown.jsx
@@ -13,11 +13,35 @@ const DropDown = ({ children, buttonText }) => {
     return (
         <>
 
-            <div className={`w-full absolute overflow-hidden transition-all duration-[1200ms] bg-red-500 ease-[cubic-bezier(0.25, 1, 0.5, 1)] ${isDrop ? "h-screen " : "h-0"} `} >
-                <div className="w-full h-full flex justify-center items-center">
-                    <ul className="flex w-full gap-[-10px] justify-center items-center  flex-col">
+            <div className={`w-full absolute overflow-hidden transition-all duration-[1200ms] overflow-y-scroll bg-red-500 ease-[cubic-bezier(0.25, 1, 0.5, 1)] ${isDrop ? "max-h-screen" : "h-0"} `} >
+                <div className="w-full flex justify-center items-center ">
+                    <ul className="flex w-full gap-[-10px] justify-center  items-center  flex-col">
                         <Link className={`${Deutschlander.className} text-[6rem] leading-none `} href="/fashion" > FASHION</Link>
                         <li className={`${Deutschlander.className} h-fit text-[6rem] leading-none`}>STREET</li>
+                        <li className={`${Deutschlander.className} h-fit text-[6rem] leading-none`}>ADVERTISING</li>
+                        <li className={`${Deutschlander.className} h-fit text-[6rem] leading-none`}>KIDS</li>
+                        <li className={`${Deutschlander.className} h-fit text-[6rem] leading-none`}>WEDDING</li>
+                        <li className={`${Deutschlander.className} h-fit text-[6rem] leading-none`}>PORTRAIT</li>
+                        <li className={`${Deutschlander.className} h-fit text-[6rem] leading-none`}>ADVERTISING</li>
+                        <li className={`${Deutschlander.className} h-fit text-[6rem] leading-none`}>KIDS</li>
+                        <li className={`${Deutschlander.className} h-fit text-[6rem] leading-none`}>WEDDING</li>
+                        <li className={`${Deutschlander.className} h-fit text-[6rem] leading-none`}>PORTRAIT</li>
+                        <li className={`${Deutschlander.className} h-fit text-[6rem] leading-none`}>ADVERTISING</li>
+                        <li className={`${Deutschlander.className} h-fit text-[6rem] leading-none`}>KIDS</li>
+                        <li className={`${Deutschlander.className} h-fit text-[6rem] leading-none`}>WEDDING</li>
+                        <li className={`${Deutschlander.className} h-fit text-[6rem] leading-none`}>PORTRAIT</li>
+                        <li className={`${Deutschlander.className} h-fit text-[6rem] leading-none`}>ADVERTISING</li>
+                        <li className={`${Deutschlander.className} h-fit text-[6rem] leading-none`}>KIDS</li>
+                        <li className={`${Deutschlander.className} h-fit text-[6rem] leading-none`}>WEDDING</li>
+                        <li className={`${Deutschlander.className} h-fit text-[6rem] leading-none`}>PORTRAIT</li>
+                        <li className={`${Deutschlander.className} h-fit text-[6rem] leading-none`}>ADVERTISING</li>
+                        <li className={`${Deutschlander.className} h-fit text-[6rem] leading-none`}>KIDS</li>
+                        <li className={`${Deutschlander.className} h-fit text-[6rem] leading-none`}>WEDDING</li>
+                        <li className={`${Deutschlander.className} h-fit text-[6rem] leading-none`}>PORTRAIT</li>
+                        <li className={`${Deutschlander.className} h-fit text-[6rem] leading-none`}>ADVERTISING</li>
+                        <li className={`${Deutschlander.className} h-fit text-[6rem] leading-none`}>KIDS</li>
+                        <li className={`${Deutschlander.className} h-fit text-[6rem] leading-none`}>WEDDING</li>
+                        <li className={`${Deutschlander.className} h-fit text-[6rem] leading-none`}>PORTRAIT</li>
                         <li className={`${Deutschlander.className} h-fit text-[6rem] leading-none`}>ADVERTISING</li>
                         <li className={`${Deutschlander.className} h-fit text-[6rem] leading-none`}>KIDS</li>
                         <li className={`${Deutschlander.className} h-fit text-[6rem] leading-none`}>WEDDING</li>
@@ -27,7 +51,7 @@ const DropDown = ({ children, buttonText }) => {
             </div>
 
             <div className=" relative overflow-hidden z-[1000px] w-full flex justify-end pr-2   ">
-                <button className="overflow-hidden block lg: top-4  rounded-l-[100px] rounded-r-[100px]  w-[80px] h-[80px]" onClick={handleDrop}>
+                <button className="overflow-hidden block lg:hidden top-4  rounded-l-[100px] rounded-r-[100px]  w-[80px] h-[80px]" onClick={handleDrop}>
                     <div className={`w-full gap-8 flex flex-col overflow-hidden justify-center items-center transition duration-700 ${isDrop ? "translate-y-[-6vh] " : "translate-y-[3.5vh]"} `}>
                         <div className={`w-full h-full overflow-hidden items-center  flex flex-col transition duration-700 ${isDrop ? "opacity-0 rotate-90" : "opacity-100 rotate-0"} `} >
                             <svg className="w-[50%]" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 32 25">
