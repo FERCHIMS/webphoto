0c29769d1a7e08ae52dad8e7827df1f10011fc19
FERCHIMS <gr.egfredderick@gmail.com>
2024-10-02 10:48:04 -0300

    arreglo de nav button

--- src/components/dropdown/DropDown.jsx
+++ src/components/dropdown/DropDown.jsx
@@ -11,9 +11,7 @@ const DropDown = ({ children, buttonText }) => {
     return (
         <>
 
-
-
-            <div className={`w-full absolute overflow-hidden h-screen transition-all duration-[1200ms] bg-red-500 ease-[cubic-bezier(0.25, 1, 0.5, 1)] ${isDrop ? "h-screen " : "h-0"} `} >
+            <div className={`w-full absolute overflow-hidden transition-all duration-[1200ms] bg-red-500 ease-[cubic-bezier(0.25, 1, 0.5, 1)] ${isDrop ? "h-screen " : "h-0"} `} >
                 <div className="w-full flex justify-center items-center">
                     <ul className="flex flex-col">
                         <li>HOLA</li>
@@ -26,21 +24,22 @@ const DropDown = ({ children, buttonText }) => {
                 </div>
             </div>
 
-            <div className="w-full flex justify-end pr-5 ">
-                <button className="relative overflow-hidden block lg:hidden top-4  rounded-l-[100px] rounded-r-[100px] z-[1000px] w-[80px] h-[80px] bg-pink-400 " onClick={handleDrop}>
-                    <div className={`flex overflow-hidden justify-center items-center gap-3 flex-col transition-all duration-[1200ms] ${isDrop ? "translate-y-[-5vh] " : "translate-y-[2vh]"} `}>
-                        <div className={`flex w-full z-10 gap-3 items-center flex-col justify-center transition-all duration-[1200ms] ${isDrop ? "" : ""} `}>
-                            <div className="bg-white w-[60%] h-[5px]"></div>
-                            <div className="bg-white w-[60%] h-[5px]"></div>
-                            <div className="bg-white w-[60%] h-[5px]"></div>
+            <div className=" relative overflow-hidden z-[1000px] w-full flex justify-end pr-5   ">
+                <button className="overflow-hidden block lg: top-4  rounded-l-[100px] rounded-r-[100px]  w-[80px] h-[80px]" onClick={handleDrop}>
+                    <div className={`w-full gap-8 flex flex-col overflow-hidden justify-center items-center transition duration-700 ${isDrop ? "translate-y-[-6vh] " : "translate-y-[3.5vh]"} `}>
+                        <div className={`w-full h-full overflow-hidden items-center  flex flex-col transition duration-700 ${isDrop ? "opacity-0 rotate-90" : "opacity-100 rotate-0"} `} >
+                            <svg className="w-[50%]" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 32 25">
+                                <path fill="#FFFFFF" d="M0 5V0h32v5zm0 10v-5h32v5zm0 10v-5h32v5z" />
+                            </svg>
                         </div>
-                        <div className={`flex w-full gap-3 items-center flex-col justify-center transition-all duration-[1200ms] ${isDrop ? "" : ""} `}>
-                            <h2 className="text-[3rem] text-white">X</h2>
+                        <div className={`flex justify-center items-center transition duration-700 ${isDrop ? "opacity-100 rotate-0" : "opacity-0 rotate-90"} `}>
+                            <svg className="w-[50%]" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 32 32">
+                                <path fill="#FFFFFF" d="m0 4 4-4 28 29-4 3z" />
+                                <path fill="#FFFFFF" d="m28 0 4 4L4 32l-4-4z" />
+                            </svg>
                         </div>
                     </div>
-
                 </button>
-
             </div>
         </>
     )
